d461c10ae654bcc5a97961cdd81f46e8cacc9f76
Dima Berry <dimaberry1@gmail.com>
2024-02-07 23:43:46 +0100

    feat: add Global.styled, Theme and Container

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
+import { Container } from "../../components/Container";
+import { FlexWrapper } from "../../components/FlexWrapper";
 
 const items = ["Home", "About", "Tech Stack", "Projects", "Contact"];
 
 export const Header = () => {
   return (
     <StyledHeader>
-      <Logo />
-      <Menu menuItems={items} />
+      <Container>
+        <FlexWrapper justify={"space-between"} align={"center"}>
+          <Logo />
+          <Menu menuItems={items} />
+        </FlexWrapper>
+      </Container>
     </StyledHeader>
   );
 };
---
+++ src/styles/Theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primaryBgr: "#191919",
+    secondaryBgr: "#252527",
+    accent: "#757205",
+
+    font: "#CCCCCC",
+  },
+};
