1ea59b09f1ef77321d4bba1174bdf37bda5ca065
Dima Berry <dimaberry1@gmail.com>
2024-02-09 23:50:25 +0100

    feat: add container in main and styled

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,19 +1,27 @@
 import styled from "styled-components";
 import photo from "./../../../assets/images/myPhoto.webp";
 import { FlexWrapper } from "../../../components/FlexWrapper";
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexWrapper align={"center"} justify={"space-around"}>
-        <div>
-          <Greeting>Hi✌️</Greeting>
-          <Name>I am Dzmitry Kaptsiuh</Name>
-          <MainTitle>A Web Developer.</MainTitle>
-        </div>
+      <Container>
+        <FlexWrapper align={"center"} justify={"space-between"}>
+          <div>
+            <Greeting>Hi✌️</Greeting>
+            <Name>
+              I am <span>Dzmitry Kaptsiuh</span>
+            </Name>
+            <MainTitle>A Web Developer.</MainTitle>
+          </div>
 
-        <Photo src={photo} alt="photo" />
-      </FlexWrapper>
+          <PhotoWrapper>
+            <Photo src={photo} alt="photo" />
+          </PhotoWrapper>
+        </FlexWrapper>
+      </Container>
     </StyledMain>
   );
 };
@@ -23,8 +31,23 @@ const StyledMain = styled.div`
   background-color: purple;
 
   display: flex;
-  justify-content: center;
-  flex-direction: column;
+`;
+
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: "";
+    width: 320px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    z-index: -1;
+  }
 `;
 
 const Photo = styled.img`
@@ -35,12 +58,31 @@ const Photo = styled.img`
 
 const MainTitle = styled.h1`
   font-size: 24px;
+  font-weight: 400;
 `;
 
 const Name = styled.h2`
   font-size: 40px;
+  font-weight: 600;
+
+  span {
+    position: relative;
+    z-index: 0;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+  }
 `;
 
 const Greeting = styled.span`
-  font-size: 24px;
+  font-size: 20px;
 `;
