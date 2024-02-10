da0ae8af6a6a48d10693d6d6f0e646a3fb5ca082
Dima Berry <dimaberry1@gmail.com>
2024-02-10 22:18:06 +0100

    feat: add styles in skills section

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -2,39 +2,60 @@ import styled from "styled-components";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Skill } from "./skill/Skill";
+import { Container } from "../../../components/Container";
 
 export const Skills = () => {
   return (
     <StyledSkills>
-      <SectionTitle>My Skills</SectionTitle>
-      <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-        <Skill
-          iconId={"html"}
-          title={"html5"}
-          description={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
-          }
-        />
-        <Skill
-          iconId={"css"}
-          title={"css"}
-          description={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
-          }
-        />
-        <Skill
-          iconId={"react"}
-          title={"react"}
-          description={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
-          }
-        />
-      </FlexWrapper>
+      <Container>
+        <SectionTitle>My Skills</SectionTitle>
+        <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+          <Skill
+            iconId={"html"}
+            title={"html5"}
+            description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
+            }
+          />
+          <Skill
+            iconId={"css"}
+            title={"css"}
+            description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
+            }
+          />
+          <Skill
+            iconId={"react"}
+            title={"react"}
+            description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
+            }
+          />
+          <Skill
+            iconId={"js"}
+            title={"js"}
+            description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
+            }
+          />
+          <Skill
+            iconId={"tailwind"}
+            title={"tailwind"}
+            description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
+            }
+          />
+          <Skill
+            iconId={"redux"}
+            title={"redux"}
+            description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?"
+            }
+          />
+        </FlexWrapper>
+      </Container>
     </StyledSkills>
   );
 };
 
-const StyledSkills = styled.section`
-  background-color: #a7b621;
-  min-height: 100vh;
-`;
+const StyledSkills = styled.section``;
