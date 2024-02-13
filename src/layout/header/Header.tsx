5be4d18ae0300240586db4bf6e728ca0ae677ee9
Dima Berry <dimaberry1@gmail.com>
2024-02-13 20:34:54 +0100

    feat: add mobile burger menu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,6 +3,7 @@ import { Logo } from "../../components/logo/Logo";
 import { Container } from "../../components/Container";
 import { FlexWrapper } from "../../components/FlexWrapper";
 import { HeaderMenu } from "./headerMenu/HeaderMeny";
+import { MobileMenu } from "./MobileMenu copy/MobileMeny";
 
 const items = ["Home", "About", "Tech Stack", "Projects", "Contact"];
 
@@ -13,6 +14,7 @@ export const Header = () => {
         <FlexWrapper justify={"space-between"} align={"center"}>
           <Logo />
           <HeaderMenu menuItems={items} />
+          <MobileMenu menuItems={items} />
         </FlexWrapper>
       </Container>
     </StyledHeader>
