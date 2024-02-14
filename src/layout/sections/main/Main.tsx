c3cd1d176f68115f7eae18d4fb0df681db32f525
Dima Berry <dimaberry1@gmail.com>
2024-02-14 19:33:26 +0100

    feat: add adaptive on main section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,12 +3,13 @@ import photo from "./../../../assets/images/myPhoto.webp";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Container } from "../../../components/Container";
 import { theme } from "../../../styles/Theme";
+import { font } from "../../../styles/Common";
 
 export const Main = () => {
   return (
     <StyledMain>
       <Container>
-        <FlexWrapper align={"center"} justify={"space-between"}>
+        <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
           <div>
             <Greeting>Hi✌️</Greeting>
             <Name>
@@ -36,6 +37,7 @@ const StyledMain = styled.div`
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 0;
+  margin-top: 65px;
 
   &::before {
     content: "";
@@ -47,6 +49,13 @@ const PhotoWrapper = styled.div`
     top: -24px;
     left: 24px;
     z-index: -1;
+
+    @media ${theme.media.mobile} {
+      width: 320px;
+      height: 470px;
+      top: -17px;
+      left: 20px;
+    }
   }
 `;
 
@@ -54,20 +63,25 @@ const Photo = styled.img`
   width: 300px;
   height: 350px;
   object-fit: cover;
+  margin-right: 35px;
+
+  @media ${theme.media.mobile} {
+    width: 320px;
+    height: 470px;
+  }
 `;
 
 const MainTitle = styled.h1`
-  font-size: 24px;
-  font-weight: 400;
+  ${font({ weight: 400, Fmax: 27, Fmin: 20 })}
 `;
 
 const Name = styled.h2`
-  font-size: 40px;
-  font-weight: 600;
+  ${font({ weight: 600, Fmax: 50, Fmin: 26 })}
 
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
 
     &::before {
       content: "";
@@ -81,8 +95,12 @@ const Name = styled.h2`
       z-index: -1;
     }
   }
+
+  @media ${theme.media.mobile} {
+    margin: 15px 0 22px;
+  }
 `;
 
 const Greeting = styled.span`
-  font-size: 20px;
+  ${font({ weight: 400, Fmax: 27, Fmin: 20 })}
 `;
