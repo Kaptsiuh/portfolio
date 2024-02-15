39cd7d95d62c5c972f140f311a5497ced35d0b4e
Dima Berry <dimaberry1@gmail.com>
2024-02-15 16:32:28 +0100

    feat: add adaptive layout to the testimony section

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,17 +4,20 @@ import { Slider } from "../../../components/slider/Slider";
 import { Icon } from "../../../components/icon/Icon";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { IconWrapper } from "../skills/skill/Skill";
+import { Container } from "../../../components/Container";
 
 export const Testimony = () => {
   return (
     <StyledTestimony>
-      <SectionTitle>Testimony</SectionTitle>
-      <FlexWrapper direction={"column"} align={"center"}>
-        <IconWrapper>
-          <Icon iconId={"react"} />
-        </IconWrapper>
-        <Slider />
-      </FlexWrapper>
+      <Container>
+        <SectionTitle>Testimony</SectionTitle>
+        <FlexWrapper direction={"column"} align={"center"}>
+          <IconWrapper>
+            <Icon iconId={"react"} />
+          </IconWrapper>
+          <Slider />
+        </FlexWrapper>
+      </Container>
     </StyledTestimony>
   );
 };
