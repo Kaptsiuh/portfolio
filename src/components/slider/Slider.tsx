99b20a02cc5fd08bbd756d70e8a9a6422a3b66a4
Dima Berry <dimaberry1@gmail.com>
2024-02-18 15:27:01 +0100

    feat: add animation on slader

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,24 +1,46 @@
 import React from "react";
-import { FlexWrapper } from "../FlexWrapper";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import { S } from "./Slider_Styles";
+import "./../../styles/slider.css";
 
-export const Slider: React.FC = () => {
+type SlidePropsType = {
+  text: string;
+  userName: string;
+};
+
+const Slide = (props: SlidePropsType) => {
   return (
-    <S.StyledSlider>
-      <FlexWrapper>
-        <S.Slide>
-          <S.Text>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
-            autem?
-          </S.Text>
-          <S.Name>Kaptsiuh</S.Name>
-        </S.Slide>
-      </FlexWrapper>
-      <S.Pagination>
-        <span></span>
-        <span className={"active"}></span>
-        <span></span>
-      </S.Pagination>
-    </S.StyledSlider>
+    <S.Slide>
+      <S.Text>{props.text}</S.Text>
+      <S.Name>{props.userName}</S.Name>
+    </S.Slide>
   );
 };
+
+const items = [
+  <Slide
+    text={
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, autem?"
+    }
+    userName={"@Kaptsiuh"}
+  />,
+  <Slide
+    text={
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, autem?"
+    }
+    userName={"@Kaptsiuh"}
+  />,
+  <Slide
+    text={
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, autem?"
+    }
+    userName={"@Kaptsiuh"}
+  />,
+];
+
+export const Slider = () => (
+  <S.StyledSlider>
+    <AliceCarousel mouseTracking items={items} />
+  </S.StyledSlider>
+);
