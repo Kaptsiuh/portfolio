1f3408386c177b86ed0fe9ca96c5372208c34d42
Dima Berry <dimaberry1@gmail.com>
2024-02-18 15:34:16 +0100

    feat: add typewriter effect on main section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,6 +3,7 @@ import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Container } from "../../../components/Container";
 import React from "react";
 import { S } from "./Main_Styles";
+import Typewriter from "typewriter-effect";
 
 export const Main: React.FC = () => {
   return (
@@ -14,7 +15,18 @@ export const Main: React.FC = () => {
             <S.Name>
               I am <span>Dzmitry Kaptsiuh</span>
             </S.Name>
-            <S.MainTitle>A Web Developer.</S.MainTitle>
+            {/* <S.MainTitle>A Web Developer.</S.MainTitle> */}
+            <S.MainTitle>
+              <p>A Web Developer.</p>
+              <Typewriter
+                options={{
+                  strings: ["A Web Developer.", "A Frontend Developer."],
+                  autoStart: true,
+                  loop: true,
+                  delay: 100,
+                }}
+              />
+            </S.MainTitle>
           </div>
 
           <S.PhotoWrapper>
--- src/layout/sections/main/Main_Styles.ts
+++ src/layout/sections/main/Main_Styles.ts
@@ -48,6 +48,10 @@ const Photo = styled.img`
 
 const MainTitle = styled.h1`
   ${font({ weight: 400, Fmax: 27, Fmin: 20 })}
+
+  p {
+    display: none;
+  }
 `;
 
 const Name = styled.h2`
